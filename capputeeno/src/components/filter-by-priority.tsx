7e645184ad1cd5b91f570c30c8ace1db712bd482
aurearibeiro <a.ribeiro@4intelligence.com.br>
2023-09-11 14:57:34 -0300

    feat: add filtering and listing products

--- capputeeno/src/components/filter-by-priority.tsx
+++ capputeeno/src/components/filter-by-priority.tsx
@@ -4,8 +4,6 @@ import { useState } from "react";
 import { useFilter } from "@/hooks/useFilter";
 import { PriorityTypes } from "@/types/priority-types";
 
-interface FilterByPriorityProps {}
-
 const FilterContainer = styled.div`
   display: flex;
   align-items: center;
@@ -35,6 +33,8 @@ const PriorityFilter = styled.ul`
   box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
   border-radius: 4px;
   padding: 12px 16px;
+  z-index: 999;
+
   list-style: none;
   top: 100%;
   li {
@@ -49,7 +49,7 @@ const PriorityFilter = styled.ul`
   }
 `;
 
-export function FilterByPriority(props: FilterByPriorityProps) {
+export function FilterByPriority() {
   const [isOpen, setIsOpen] = useState(false);
   const { setPriority } = useFilter();
 
